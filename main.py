d5b66bdfafc4ae71746ecc77efb29bb9ac610eb7
Anh Vo <votqanh@gmail.com>
2021-06-02 15:40:21 +0630

    Wait for confirmation page

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import datetime
-from multiprocessing import cpu_count, Pool
 import time
+from multiprocessing import Pool
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -15,26 +15,12 @@ import config
 
 
 def process(person):
-    print(f'Booking for {person[0]}...')
+    print(f'\nBooking for {person[0]}...')
 
     # if times:
     #     book(person, times)
     # else:
     book(person)
-
-
-def submitted(driver, person):
-    if driver.find_elements_by_xpath('//*[@id="gform_submit_button_68"]'):
-        print(f'\n{person[0]}', end=': ')
-
-        if driver.find_elements_by_css_selector('div[class="validation_error"]'):
-            print('Cannot book twice')
-        else:
-            print('Successful booking')
-
-        return True
-    else:
-        return False
 
 
 def book(data, time_slot=['4:00 PM', '4:30 PM']):
@@ -59,32 +45,32 @@ def book(data, time_slot=['4:00 PM', '4:30 PM']):
             Select(driver.find_element_by_xpath('//*[@id="input_68_7"]')).select_by_value(value)
 
             today = datetime.date.today()
-            WebDriverWait(driver, 20).until(EC.invisibility_of_element_located(
+            WebDriverWait(driver, 40).until(EC.invisibility_of_element_located(
                 (By.CSS_SELECTOR, 'div[class="ga_monthly_schedule_wrapper ga_spinner"]')))
 
-            driver.find_element_by_css_selector(f'td[date-go="{today.strftime("%Y-%m-%d")}"]').click()
-            time.sleep(1)
+            driver.find_element_by_css_selector(f'td[date-go="{today.strftime("%Y-%m-%-d")}"]').click()
+            time.sleep(0.5)
 
-            slot = f'{today.strftime("%A, %B %d %Y")} at '
+            slot = f'{today.strftime("%A, %B %-d %Y")} at '
             cnt = 0
 
-            print('')
+            # print('')
 
             for t in time_slot:
                 el = driver.find_elements_by_css_selector(f'label[lang_slot="{slot + t}"]')
-                print(f'{data[0]}: {t}', end=' ')
+                # print(f'{data[0]}: {t}', end=' ')
 
                 if el:
                     driver.execute_script("arguments[0].click();", el[0])
 
-                    if driver.find_elements_by_xpath('//*[@id="ga_selected_bookings"]/div'):
-                        print('has been selected')
-                    else:
-                        cnt += 1
-                        print('is unavailable')
+                    # if driver.find_elements_by_xpath('//*[@id="ga_selected_bookings"]/div'):
+                    #     print('has been selected')
+                    # else:
+                    #     cnt += 1
+                    #     print('is unavailable')
                 else:
                     cnt += 1
-                    print('is unavailable')
+                    # print('is unavailable')
 
             if cnt == len(time_slot):
                 print(data[0], ': Could not book because both requested timeslots are unavailable')
@@ -93,17 +79,32 @@ def book(data, time_slot=['4:00 PM', '4:30 PM']):
             driver.find_element_by_xpath('//*[@id="gform_submit_button_68"]').click()
 
             # wait for confirmation
-            while not submitted(driver, data):
+            while not driver.find_elements_by_xpath('//*[@id="gform_submit_button_68"]'):
                 pass
 
+            while not driver.find_elements_by_xpath('//*[@id="post-1608"]/div/div/div[1]/div/h1'):
+                pass
+
+            print(f'\n{data[0]}', end=': ')
+
+            if driver.find_elements_by_id('gform_confirmation_message_68'):
+                print('Successful booking')
+            else:
+                print('Cannot book twice')
+
         except Exception as e:
-            print(data[0], ': Could not book because:')
+            print(f'{data[0]}: Could not book because')
             print(e)
 
 
 print("\nISY GYM BOOKER")
 
 dont_book = [x.capitalize() for x in input("\nIs anyone not going today?\n").split()]
+
+if dont_book:
+    print('')
+
+print('Default timeslots: 4:00 PM and 4:30 PM')
 
 # print('\nWhen do you want to go? Enter 1 time slot at a time or leave blank for default 4:00 PM and 4:30 PM.')
 #
